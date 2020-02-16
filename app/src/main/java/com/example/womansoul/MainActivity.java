03d909b056a9d8947342c25494f027076d8f11df
El-Ectron <33848027+El-Ectron@users.noreply.github.com>
2020-02-16 13:29:45 -0600

    h

--- app/src/main/java/com/example/womansoul/MainActivity.java
+++ app/src/main/java/com/example/womansoul/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validar(correo.getText().toString(),pass.getText().toString());
+                validar(correo.getText().toString(),pass.getText().toString());h
             }
         });
         create.setOnClickListener(new View.OnClickListener() {
